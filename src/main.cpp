65715c79a4ec8fb6fd680b24460d3ee851e2c5f0
gnayuy <yangy@ibs-yangy-ux1.corp.alleninstitute.org>
2018-06-08 10:18:47 -0700

    update a release version

--- src/main.cpp
+++ src/main.cpp
@@ -12,7 +12,7 @@
 # include <stdlib.h>
 #include <iostream>
 
-#include "cxxopts.hpp"
+//#include "cxxopts.hpp"
 #include "BigTree.h"
 
 //
@@ -37,65 +37,67 @@ int main (int argc, const char *argv[])
     int genMetaInfo = 0; // write mdata.bin
     bool genZeroData = false;
 
+    BigTree bigtree(argv[1], argv[2], atoi(argv[3]), genMetaInfo, genZeroData);
+
     //
-    try
-    {
-        cxxopts::Options options(argv[0], "bigtree -i <input_DIR> -o <output_DIR> -n <Number_of_Resolutions_of_TMITREE>");
-        options
-                .positional_help("[optional args]")
-                .show_positional_help();
+//    try
+//    {
+//        cxxopts::Options options(argv[0], "bigtree -i <input_DIR> -o <output_DIR> -n <Number_of_Resolutions_of_TMITREE>");
+//        options
+//                .positional_help("[optional args]")
+//                .show_positional_help();
 
-        options.add_options()
-                ("h,help", "BigTree Version 1.0")
-                ("i,input", "Input DIR", cxxopts::value<std::string>(inputDir))
-                ("o,output", "Output DIR", cxxopts::value<std::string>(outputDir))
-                ("n,resolutions", "N Resolutions", cxxopts::value<int>(nScale))
-                ("m,meta", "generate meta info only", cxxopts::value<int>(genMetaInfo))
-                ("z,zeros", "generate zero tiff images", cxxopts::value<bool>(genZeroData))
-                ;
+//        options.add_options()
+//                ("h,help", "BigTree Version 1.0")
+//                ("i,input", "Input DIR", cxxopts::value<std::string>(inputDir))
+//                ("o,output", "Output DIR", cxxopts::value<std::string>(outputDir))
+//                ("n,resolutions", "N Resolutions", cxxopts::value<int>(nScale))
+//                ("m,meta", "generate meta info only", cxxopts::value<int>(genMetaInfo))
+//                ("z,zeros", "generate zero tiff images", cxxopts::value<bool>(genZeroData))
+//                ;
 
-        auto cmds = options.parse(argc, argv);
+//        auto cmds = options.parse(argc, argv);
 
-        if (cmds.count("help"))
-        {
-            std::cout << options.help({""}) << std::endl;
-            exit(0);
-        }
+//        if (cmds.count("help"))
+//        {
+//            std::cout << options.help({""}) << std::endl;
+//            exit(0);
+//        }
 
-        if (cmds.count("input"))
-        {
-            std::cout << " -- Input 2D TIFF Images DIR: " << cmds["input"].as<std::string>() << std::endl;
-        }
+//        if (cmds.count("input"))
+//        {
+//            std::cout << " -- Input 2D TIFF Images DIR: " << cmds["input"].as<std::string>() << std::endl;
+//        }
 
-        if (cmds.count("output"))
-        {
-            std::cout << " -- Output 3D TIFF Images DIR: " << cmds["output"].as<std::string>() << std::endl;
-        }
+//        if (cmds.count("output"))
+//        {
+//            std::cout << " -- Output 3D TIFF Images DIR: " << cmds["output"].as<std::string>() << std::endl;
+//        }
 
-        if (cmds.count("n"))
-        {
-            std::cout << " -- Convert data into " << cmds["resolutions"].as<int>() << " scales" << std::endl;
-        }
+//        if (cmds.count("n"))
+//        {
+//            std::cout << " -- Convert data into " << cmds["resolutions"].as<int>() << " scales" << std::endl;
+//        }
 
-        if (cmds.count("m"))
-        {
-            std::cout << " -- Generate mdata.bin " << cmds["meta"].as<int>() << std::endl;
-        }
+//        if (cmds.count("m"))
+//        {
+//            std::cout << " -- Generate mdata.bin " << cmds["meta"].as<int>() << std::endl;
+//        }
 
-        if (cmds.count("z"))
-        {
-            std::cout << " -- Generate zero tiff images " << cmds["zeros"].as<bool>() << std::endl;
-        }
+//        if (cmds.count("z"))
+//        {
+//            std::cout << " -- Generate zero tiff images " << cmds["zeros"].as<bool>() << std::endl;
+//        }
 
-    }
-    catch(const cxxopts::OptionException& e)
-    {
-        std::cout << "error parsing options: " << e.what() << std::endl;
-        exit(1);
-    }
+//    }
+//    catch(const cxxopts::OptionException& e)
+//    {
+//        std::cout << "error parsing options: " << e.what() << std::endl;
+//        exit(1);
+//    }
 
-    // BigTree
-    BigTree bigtree(inputDir, outputDir, nScale, genMetaInfo, genZeroData);
+//    // BigTree
+//    BigTree bigtree(inputDir, outputDir, nScale, genMetaInfo, genZeroData);
 
     //
     return 0;
